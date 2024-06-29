6502701fd4dd044aad5bbbc8a0beedc7d6a00378
Chandan Maharana <chandanmaharana144@gmail.com>
2024-06-30 00:12:56 +0530

    Lecture 8

---
+++ Lecture 8/Loops.java
@@ -0,0 +1,27 @@
+
+public class Loops {
+    public static void main(String[] args) {
+        int i;
+
+        //for loop
+        for(i=1;i<=100;i++)
+        {
+            System.out.println(i);
+        }
+        System.out.println(i);
+
+        System.out.println("**********************");
+
+        int n=5;
+        // while(n>0){//infinite loop
+        //     System.out.println("hi");
+        // }
+        System.out.println("**********************");
+
+
+        //do-while loop
+        do{
+            System.out.println(n);
+        }while(n>0);
+    }
+}
